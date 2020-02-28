c312008b65d59383a7209b324fc96b806adaa3ce
KaterinaZV <ekaterinazvereva@MacBook-Pro-Ekaterina.local>
2020-02-28 18:29:31 +0300

    Fix homework

--- homework1.js
+++ homework1.js
@@ -2,7 +2,7 @@
 
 const readline = require('readline');
 
-const rl = readline.createInterface({ input: process.stdin , output: process.stdout });
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 const getLine = (function () {
     const getLineGen = (async function* () {
@@ -16,7 +16,7 @@ const getLine = (function () {
 const phoneBook = {};
 
 // Добавление имен и номеров
-async function add(){
+async function add() {
     console.log('Введите имя');
     const name = await getLine();
     console.log('Введите номер');
@@ -26,20 +26,25 @@ async function add(){
 }
 
 // Печать контактов
-async function print (){
-     console.log(phoneBook);
- }
+async function print() {
+    console.log(phoneBook);
+}
 
- // Удаление номеров
- async function deletePhone(){
-     console.log('Введите имя для удаления');
-     const name =  await getLine();
+// Удаление номеров
+async function deletePhone() {
+    console.log('Введите имя для удаления');
+    const name = await getLine();
 
-    delete phoneBook[name];
- }
+    if (name in phoneBook) {
+        delete phoneBook[name];
+        console.log("Контакт удален");
+    } else {
+        console.log("Такого имени нет в базе");
+    }
+}
 
- // Поиск номера по имени
- async function searchPhone(){
+// Поиск номера по имени
+async function searchPhone() {
     console.log('Какое имя нужно найти?');
     const name = await getLine();
 
@@ -48,20 +53,20 @@ async function print (){
     } else {
         console.log("Такого имени нет в базе");
     }
- }
+}
 
 const main = async () => {
     console.log(`Введите команду add- добавить контакт, \n print/ показать контакты, delete/ удалить контакт, \n search/ поиск контакта. Для выхода используйте команду - exit`);
     const command = await getLine();
     if (command === 'exit') {
         process.exit(0);
-    } else if (command === 'add'){
+    } else if (command === 'add') {
         await add();
-    } else if (command === 'print'){
+    } else if (command === 'print') {
         await print();
     } else if (command === 'delete') {
         await deletePhone();
-    } else if (command === 'search'){
+    } else if (command === 'search') {
         await searchPhone();
     } else {
         console.log('Неизвестная команда');
